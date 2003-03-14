170df41521b3a4d4d02fd69c1d5ac4829edcfeeb
Garret Wilson <garret@globalmentor.com>
2003-03-14 01:30:39 +0000

    created text/directory parser value factory framework

--- src/com/garretwilson/text/directory/DirectoryProcessor.java
+++ src/com/garretwilson/text/directory/DirectoryProcessor.java
@@ -17,10 +17,84 @@ import com.garretwilson.util.*;
 	defined in 
 	<a href="http://www.ietf.org/rfc/rfc2425.txt">RFC 2425</a>,
 	"A MIME Content-Type for Directory Information".
+<p>The processor knows how to process
+	the standard directory value types: <code>URI_VALUE_TYPE</code>,
+	<code>TEXT_VALUE_TYPE</code>, <code>DATE_VALUE_TYPE</code>,
+	<code>TIME_VALUE_TYPE</code>, <li><code>DATE_TIME_VALUE_TYPE</code>,
+	<code>INTEGER_VALUE_TYPE</code>, <code>BOOLEAN_VALUE_TYPE</code>,
+	and <code>FLOAT_VALUE_TYPE</code>.</p>
 @author Garret Wilson
+@see ValueFactory
+@see URI_VALUE_TYPE
+@see TEXT_VALUE_TYPE
+@see DATE_VALUE_TYPE
+@see TIME_VALUE_TYPE
+@see DATE_TIME_VALUE_TYPE
+@see INTEGER_VALUE_TYPE
+@see BOOLEAN_VALUE_TYPE
+@see FLOAT_VALUE_TYPE
 */
-public class DirectoryProcessor implements DirectoryConstants
+public class DirectoryProcessor implements DirectoryConstants, ValueFactory
 {
+	
+	/**A map of value factories keyed to the lowercase version of the value type.*/
+	private final Map valueTypeValueFactoryMap=new HashMap();	
+
+		/**Registers a value factory by value type.
+		@param valueType The value type for which this value factory can create values.
+		@param valueFactory The value factory to be registered with this value type.
+		*/	
+		public void registerValueFactoryByValueType(final String valueType, final ValueFactory valueFactory)
+		{
+			valueTypeValueFactoryMap.put(valueType.toLowerCase(), valueFactory);	//put the value factory in the map, keyed to the lowercase version of the type
+		}
+		
+		/**Retrieves a value factory to create values for the given value type.
+		@param valueType The value type for which a value factory should be returned.
+		@return A value factory for this value type, or <code>null</code> if there
+			is no value factory registered for this value type.
+		*/ 
+		protected ValueFactory getValueFactoryByValueType(final String valueType)
+		{
+			return (ValueFactory)valueTypeValueFactoryMap.get(valueType.toLowerCase());	//get the value factory keyed to the lowercase version of this value type
+		}
+		
+	/**A map of value factories keyed to the lowercase version of the profile.*/
+	private final Map profileValueFactoryMap=new HashMap();	
+
+		/**Registers a value factory by profile.
+		@param profile The profile for which this value factory can create values.
+		@param valueFactory The value factory to be registered with this profile.
+		*/	
+		public void registerValueFactoryByProfile(final String profile, final ValueFactory valueFactory)
+		{
+			profileValueFactoryMap.put(profile.toLowerCase(), valueFactory);	//put the value factory in the map, keyed to the lowercase version of the profile
+		}
+
+		/**Retrieves a value factory to create values for the given value profile.
+		@param profile The profile for which a value factory should be returned.
+		@return A value factory for this profile, or <code>null</code> if there
+			is no value factory registered for this profile.
+		*/ 
+		protected ValueFactory getValueFactoryByProfile(final String profile)
+		{
+			return (ValueFactory)profileValueFactoryMap.get(profile.toLowerCase());	//get the value factory keyed to the lowercase version of this profile
+		}
+		
+	/**Default constructor.
+	This class automatically registers itself with itself as a value factory for
+		the standard value types.*/
+	public DirectoryProcessor()
+	{
+		registerValueFactoryByValueType(URI_VALUE_TYPE, this);	//register ourselves as a value factory for the standard value types		
+		registerValueFactoryByValueType(TEXT_VALUE_TYPE, this);		
+		registerValueFactoryByValueType(DATE_VALUE_TYPE, this);		
+		registerValueFactoryByValueType(TIME_VALUE_TYPE, this);		
+		registerValueFactoryByValueType(DATE_TIME_VALUE_TYPE, this);		
+		registerValueFactoryByValueType(INTEGER_VALUE_TYPE, this);		
+		registerValueFactoryByValueType(BOOLEAN_VALUE_TYPE, this);		
+		registerValueFactoryByValueType(FLOAT_VALUE_TYPE, this);		
+	}
 
 	/**The delimiter characters separating the main components of a content line
 		with no group provided (';', ':', CR, and LF).
@@ -149,8 +223,69 @@ public class DirectoryProcessor implements DirectoryConstants
 		one or more object representing the value, as some value types
 		support multiple values.
 	<p>Whatever delimiter ended the value will be left in the reader.</p>
-	<p>The following predefined types, along with the objects returned, are as
-		follows:</p>
+	<p>When attempting to find a <code>ValueFactory</code> to process a given
+		value, an attempt is made to locate a value factory based upon, in this
+		order:</p>
+	<ul>
+		<li>value type; if there is no value type, or no value was created, then</li>
+		<li>profile</li>
+	</ul> 
+	<p>If the value cannot be created using a <code>ViewFactory</code>, the
+		value is converted to a single string.</p>
+	@param group The group specification, or <code>null</code> if there is no group.
+	@param name The name of the information.
+	@param paramList The list of parameters, each item of which is a
+		<code>NameValuePair</code> with a name of type <code>String</code> and a
+		value of type <code>Object</code>.
+	@param reader The reader that contains the lines of the directory.
+	@return An array of objects represent the value string.
+	@exception IOException Thrown if there is an error reading the directory.
+	@exception ParseIOException Thrown if there is a an error interpreting the directory.
+	@see NameValuePair
+	*/
+	protected Object[] processValue(final String group, final String name, final List paramList, final LineUnfoldParseReader reader) throws IOException, ParseIOException
+	{
+		String valueType=null;	//start out not knowing what the value type will be
+		final Iterator paramIterator=paramList.iterator();	//get an iterator to the parameters
+		while(paramIterator.hasNext())	//while there are more parameters
+		{
+			final NameValuePair parameter=(NameValuePair)paramIterator.next();	//get the next parameter name/value pair
+			if(VALUE_PARAM_NAME.equals(parameter.getName()))	//if this is the "value" parameter
+			{
+				valueType=(String)parameter.getValue();	//get the value type
+				break;	//stop looking for a value type
+			}
+		}
+		Object[] objects=null;	//start out by assuming we can't process the value
+		final ValueFactory valueTypeValueFactory=getValueFactoryByValueType(valueType);	//see if we have a value factory registered with this value type
+		if(valueTypeValueFactory!=null)	//if there is a value factory for this value type
+		{
+			objects=valueTypeValueFactory.createValue(group, name, paramList, valueType, reader);	//create objects for this value type
+		}
+		if(objects==null)	//if no objects were created
+		{
+/*G***fix looking up by profile			
+			final ValueFactory profileValueFactory=getValueFactoryByProfile(profile);	//see if we have a value factory registered with this profile
+			if(profileValueFactory!=null)	//if there is a value factory for this profile
+			{
+				objects=profileValueFactory.createValue(group, name, paramList, valueType, reader);	//create objects for this profile
+			}
+*/
+		}
+		if(objects==null)	//if no objects were created
+		{
+			final String valueString=reader.readStringUntilChar(CR);	//everything before the carriage return will constitute the value
+			objects=new String[]{valueString};	//put the single value string in an array of strings and use that for the value objects
+		}
+		return objects;	//return the value objects we processed
+	}
+
+	/**Processes the textual representation of a line's value and returns
+		one or more object representing the value, as some value types
+		support multiple values.
+	<p>Whatever delimiter ended the value will be left in the reader.</p>
+	<p>This method knows how to create predefined types, which,
+		along with the objects returned, are as follows:</p>
 	<ul>
 		<li><code>URI_VALUE_TYPE</code> <code>URI</code></li>
 		<li><code>TEXT_VALUE_TYPE</code> <code>String</code></li>
@@ -160,14 +295,18 @@ public class DirectoryProcessor implements DirectoryConstants
 		<li><code>INTEGER_VALUE_TYPE</code> <code>Integer</code></li>
 		<li><code>BOOLEAN_VALUE_TYPE</code> <code>Boolean</code></li>
 		<li><code>FLOAT_VALUE_TYPE</code> <code>Double</code></li>
-	</ul>		
+	</ul>
 	@param group The group specification, or <code>null</code> if there is no group.
 	@param name The name of the information.
 	@param paramList The list of parameters, each item of which is a
 		<code>NameValuePair</code> with a name of type <code>String</code> and a
 		value of type <code>Object</code>.
+	@param valueType The type of value, or <code>null</code> if the type of value
+		is unknown.
 	@param reader The reader that contains the lines of the directory.
-	@return An array of objects represent the value string.
+	@return An array of objects represent the value string, or <code>null</code>
+		if the type of value cannot be determined by the given line information,
+		in which case no information is removed from the reader.
 	@exception IOException Thrown if there is an error reading the directory.
 	@exception ParseIOException Thrown if there is a an error interpreting the directory.
 	@see NameValuePair
@@ -186,34 +325,15 @@ public class DirectoryProcessor implements DirectoryConstants
 	@see FLOAT_VALUE_TYPE
 	@see Double
 	*/
-	public Object[] processValue(final String group, final String name, final List paramList, final LineUnfoldParseReader reader) throws IOException, ParseIOException
+	public Object[] createValue(final String group, final String name, final List paramList, final String valueType, final LineUnfoldParseReader reader) throws IOException, ParseIOException
 	{
-//G***del		final List valueList=new ArrayList();	//create a new list to hold the objects we find
-		String valueType=null;	//start out not knowing what the value type will be
-		final Iterator paramIterator=paramList.iterator();	//get an iterator to the parameters
-		while(paramIterator.hasNext())	//while there are more parameters
+		if(TEXT_VALUE_TYPE.equalsIgnoreCase(valueType))	//if this is the "text" value type
 		{
-			final NameValuePair parameter=(NameValuePair)paramIterator.next();	//get the next parameter name/value pair
-			if(VALUE_PARAM_NAME.equals(parameter.getName()))	//if this is the "value" parameter
-			{
-				valueType=(String)parameter.getValue();	//get the value type
-				break;	//stop looking for a value type
-			}
+			return processTextValueList(reader);	//process the text value
 		}
-		if(TEXT_VALUE_TYPE.equals(valueType))	//if this is the "text" value type
-		{
-			return processTextValue(reader);	//process the text value
-		}
-		
-		
-		return null;	//G***fix
+		return null;	//show that we can't create a value
 	}
 
-
-	/**The delimiters that can divide a text value: '\\' ',' and CR.*/
-	protected final static String TEXT_VALUE_DELIMITER_CHARS=""+TEXT_ESCAPE_CHAR+VALUE_SEPARATOR_CHAR+CR; 
-	
-	
 	/**Processes a text value.
 	<p>The sequence "\n" or "\N" will be converted to a single newline character,
 		'\n'.</p>
@@ -223,19 +343,46 @@ public class DirectoryProcessor implements DirectoryConstants
 	@exception IOException Thrown if there is an error reading the directory.
 	@exception ParseIOException Thrown if there is a an error interpreting the directory.
 	*/
-	protected String[] processTextValue(final LineUnfoldParseReader reader) throws IOException, ParseIOException
+	protected String[] processTextValueList(final LineUnfoldParseReader reader) throws IOException, ParseIOException
 	{
 		final List stringList=new ArrayList();	//create a new list to hold the strings we find
-//G***del when works		final StringBuffer stringBuffer=new StringBuffer(valueString);	//create a string buffer to parse the string value
+		char delimiter;	//we'll store the last delimiter peeked		
+		do
+		{
+			reader.resetPeek();	//reset peeking
+			final String string=processTextValue(reader);	//read a string
+			delimiter=reader.peekChar();	//see what character is next
+		}
+		while(delimiter==VALUE_SEPARATOR_CHAR);	//keep getting strings while we are still running into value separators
+		reader.resetPeek();	//reset peeking
+		return (String[])stringList.toArray(new String[stringList.size()]);	//convert the list of strings to an array of strings and return the array
+	}
+
+	/**The delimiters that can divide a text value: '\\' ',' and CR.*/
+	protected final static String TEXT_VALUE_DELIMITER_CHARS=""+TEXT_ESCAPE_CHAR+VALUE_SEPARATOR_CHAR+CR; 
+
+	/**Processes a single text value.
+	<p>The sequence "\n" or "\N" will be converted to a single newline character,
+		'\n'.</p>
+	<p>Whatever delimiter ended the value will be left in the reader.</p>
+	@param reader The reader that contains the lines of the directory.
+	@return An array of strings representing the values.
+	@exception IOException Thrown if there is an error reading the directory.
+	@exception ParseIOException Thrown if there is a an error interpreting the directory.
+	*/
+	protected String processTextValue(final LineUnfoldParseReader reader) throws IOException, ParseIOException
+	{
 		final StringBuffer stringBuffer=new StringBuffer();	//create a string buffer to hold whatever string we're processing
-		while(true)	//keep collecting strings until we encounter a CR, at which point we'll return the value
+		char delimiter;	//we'll store the last delimiter peeked		
+		do	
 		{		
 			stringBuffer.append(reader.readStringUntilChar(TEXT_VALUE_DELIMITER_CHARS));	//read all undelimited characters until we find a delimiter
-			final char delimiter=reader.readChar();	//read the next character, the delimiter we found
+			delimiter=reader.peekChar();	//see what the delimiter will be
 			switch(delimiter)	//see which delimiter we found
 			{
 				case TEXT_ESCAPE_CHAR:	//if this is an escape character ('\\')
 					{
+						reader.skip(1);	//skip the delimiter
 						final char escapedChar=reader.readChar();	//read the character after the escape character
 						switch(escapedChar)	//see what character comes after this one
 						{
@@ -252,18 +399,16 @@ public class DirectoryProcessor implements DirectoryConstants
 					}
 					break;
 				case VALUE_SEPARATOR_CHAR:	//if this is the character separating multiple values (',')
-					stringList.add(stringBuffer.toString());	//add the string we've collected to the list of strings
-					stringBuffer.delete(0, stringBuffer.length());	//remove the contents of the string buffer
-					break;
 				case CR:	//if we just read a carriage return
-					stringList.add(stringBuffer.toString());	//add the string we've collected to the list of strings
-					reader.unread(delimiter);	//unread the carriage return
-					//G***check the text value
-					return (String[])stringList.toArray(new String[stringList.size()]);	//convert the list of strings to an array of strings and return the array
+					break;	//don't do anything---we'll just collect our characters and leave
 				default:	//if we read anything else (there shouldn't be anything else unless there is a logic error)					
 					throw new ParseUnexpectedDataException(TEXT_VALUE_DELIMITER_CHARS, delimiter, reader);	//show that we didn't expect this character here
 			}
 		}
+		while(delimiter!=VALUE_SEPARATOR_CHAR && delimiter!=CR);	//keep collecting parts of the string until we encounter a ',' or a CR
+		//G***check the text value
+		reader.resetPeek();	//reset peeking
+		return stringBuffer.toString();	//return the string we've collected so far
 	}
 
 }
---
+++ src/com/garretwilson/text/directory/ValueFactory.java
@@ -0,0 +1,36 @@
+package com.garretwilson.text.directory;
+
+import java.io.*;
+import java.util.*;
+import com.garretwilson.io.*;
+
+/**Class that knows how to create objects to represent values provided in
+	the lines of a <code>text/directory</code> as defined in 
+	<a href="http://www.ietf.org/rfc/rfc2425.txt">RFC 2425</a>,
+	"A MIME Content-Type for Directory Information".
+@author Garret Wilson
+*/
+public interface ValueFactory
+{
+	
+	/**Processes the textual representation of a line's value and returns
+		one or more object representing the value(s).
+	<p>Whatever delimiter ended the value will be left in the reader.</p>
+	@param group The group specification, or <code>null</code> if there is no group.
+	@param name The name of the information.
+	@param paramList The list of parameters, each item of which is a
+		<code>NameValuePair</code> with a name of type <code>String</code> and a
+		value of type <code>Object</code>.
+	@param valueType The type of value, or <code>null</code> if the type of value
+		is unknown.
+	@param reader The reader that contains the lines of the directory.
+	@return An array of objects represent the value string, or <code>null</code>
+		if the type of value cannot be determined by the given line information,
+		in which case no information is removed from the reader.
+	@exception IOException Thrown if there is an error reading the directory.
+	@exception ParseIOException Thrown if there is a an error interpreting the directory.
+	@see NameValuePair
+	*/	
+	public Object[] createValue(final String group, final String name, final List paramList, final String valueType, final LineUnfoldParseReader reader) throws IOException, ParseIOException;
+	
+}
